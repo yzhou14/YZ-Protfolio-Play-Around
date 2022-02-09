44ff5beaf03df4ee763a4bdac4c2ab37bd98540c
yzhou14 <yzhou14@risd.edu>
2022-02-09 11:22:54 -0500

    update

---
+++ sketch02.js
@@ -0,0 +1,13 @@
+//sketch 01
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255, 255, 255);
+
+  stroke(255, 178, 102)
+  strokeWeight(2)
+  fill(255, 255, 204)
+  ellipse(mouseX, mouseY, 120, 120)
+}
